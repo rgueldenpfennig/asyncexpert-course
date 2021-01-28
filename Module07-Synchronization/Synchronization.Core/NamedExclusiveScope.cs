cba3770e07d27e5d084aab880e81b0f674da9953
rgueldenpfennig <mail@robin-gueldenpfennig.de>
2021-01-28 17:21:24 +0100

    Complete homework of Module07-Synchronization

--- Module07-Synchronization/Synchronization.Core/NamedExclusiveScope.cs
+++ Module07-Synchronization/Synchronization.Core/NamedExclusiveScope.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Threading;
 
 namespace Synchronization.Core
@@ -13,8 +14,94 @@ namespace Synchronization.Core
      */
     public class NamedExclusiveScope : IDisposable
     {
+        private bool _isDisposed;
+        private readonly string _name;
+        private readonly Semaphore _semaphore;
+
+        private static readonly ConcurrentDictionary<string, NamedExclusiveScope> _instances =
+            new ConcurrentDictionary<string, NamedExclusiveScope>();
+
+        private int _count = 0;
+
         public NamedExclusiveScope(string name, bool isSystemWide)
         {
+            _name = name;
+
+            if (isSystemWide)
+            {
+                _semaphore = new Semaphore(1, 1, name, out bool createdNew);
+                if (!createdNew) throw new InvalidOperationException($"Unable to get a global lock {name}.");
+
+                _semaphore.WaitOne();
+            }
+            else
+            {
+                if (_instances.TryGetValue(name, out var instance))
+                {
+                    Interlocked.Increment(ref instance._count);
+                    instance._semaphore.WaitOne();
+                }
+                else
+                {
+                    _semaphore = new Semaphore(1, 1);
+                    instance = _instances.GetOrAdd(name, this);
+
+                    if (ReferenceEquals(this, instance))
+                    {
+                        Interlocked.Increment(ref _count);
+                        _semaphore.WaitOne();
+                    }
+                    else
+                    {
+                        _semaphore.Dispose();
+
+                        Interlocked.Increment(ref instance._count);
+                        instance._semaphore.WaitOne();
+                    }
+                }
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    if (_instances.TryGetValue(_name, out var instance))
+                    {
+                        Interlocked.Decrement(ref instance._count);
+                        instance._semaphore.Release();
+                        
+                        if (Interlocked.CompareExchange(ref instance._count, 0, 0) == 0)
+                        {
+                            instance._semaphore.Dispose();
+                        }
+                    }
+                    else
+                    {
+                        _semaphore.Dispose();
+                    }
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                _isDisposed = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~NamedExclusiveScope()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
--- Module07-Synchronization/Synchronization.Tests/UnitTest1.cs
+++ Module07-Synchronization/Synchronization.Tests/UnitTest1.cs
@@ -1,12 +1,69 @@
 using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Synchronization.Core;
 using Xunit;
 
 namespace Synchronization.Tests
 {
     public class UnitTest1
     {
+        [Fact]
+        public void NamedExclusiveScope_CanBeUsedSystemWide()
+        {
+#pragma warning disable CS0642 // Possible mistaken empty statement
+            using (new NamedExclusiveScope(Guid.NewGuid().ToString(), true)) ;
+#pragma warning restore CS0642 // Possible mistaken empty statement
+        }
+
+        [Fact]
+        public async Task NamedExclusiveScope_WhenMultipleLocalExclusiveScopeInSameProcess_ThenSucceeds()
+        {
+            var tasks = Enumerable.Range(0, 5).Select(i => Task.Run(() =>
+            {
+                using (new NamedExclusiveScope("name", false))
+                {
+                    Thread.Sleep(300);
+                    return DateTime.Now;
+                }
+            })).ToArray();
+
+            await Task.WhenAll(tasks);
+
+            var minFinishTime = tasks.Select(t => t.Result).Min();
+            var maxFinishTime = tasks.Select(t => t.Result).Max();
+            var duration = (maxFinishTime - minFinishTime).TotalMilliseconds;
+
+            Assert.True(duration >= 1200, "execution wasn't sequencial");
+        }
+
+        [Fact]
+        public async Task NamedExclusiveScope_WhenMultipleLocalExclusiveScopesWithUniqueNamesInSameProcess_ThenSucceeds()
+        {
+            var tasks = Enumerable.Range(0, 5).Select(i =>
+            {
+                var index = i;
+                return Task.Run(() =>
+                {
+                    using (new NamedExclusiveScope($"name {index}", false))
+                    {
+                        Thread.Sleep(300);
+                        return DateTime.Now;
+                    }
+                });
+            }).ToArray();
+
+            await Task.WhenAll(tasks);
+
+            var minFinishTime = tasks.Select(t => t.Result).Min();
+            var maxFinishTime = tasks.Select(t => t.Result).Max();
+            var duration = (maxFinishTime - minFinishTime).TotalMilliseconds;
+
+            Assert.True(duration < 1200, "execution wasn't parallel");
+        }
+
         [Fact]
         public async Task GivenExampleApp_WhenLocalExclusiveScope_ThenSucceeds()
         {
@@ -28,6 +85,19 @@ namespace Synchronization.Tests
         }
 
         [Fact]
+        public async Task GivenExampleApp_WhenTwoLocalExclusiveScopesInDifferentProcesses_ThenSucceeds()
+        {
+            var scopeName = "someScopeName";
+            var path = @"..\..\..\..\..\Synchronization\bin\x64\Debug\netcoreapp3.1\Synchronization.exe";
+            var firstRunTask = RunProgramAsync(path, $"{scopeName} false");
+            var exception = await Record.ExceptionAsync(async () =>
+                await RunProgramAsync(path, $"{scopeName} false"));
+            await firstRunTask;
+
+            Assert.Null(exception);
+        }
+
+        [Fact]
         public async Task GivenExampleApp_WhenDoubleGlobalExclusiveScope_ThenThrows()
         {
             var scopeName = "someScopeName";
@@ -39,12 +109,12 @@ namespace Synchronization.Tests
 
             Assert.NotNull(exception);
             Assert.IsType<Exception>(exception);
-            Assert.StartsWith($"Unhandled exception. System.InvalidOperationException: Unable to get a global lock {scopeName}.", 
+            Assert.StartsWith($"Unhandled exception. System.InvalidOperationException: Unable to get a global lock {scopeName}.",
                 exception.Message);
 
         }
 
-        public static Task<string> RunProgramAsync(string path, string args = "")
+        private static Task<string> RunProgramAsync(string path, string args = "")
         {
             var tcs = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
 
