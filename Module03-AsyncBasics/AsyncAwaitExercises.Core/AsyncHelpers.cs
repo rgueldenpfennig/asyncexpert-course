2d97cb1f51442b28c70dec58e260eb27b0930fdd
rgueldenpfennig <mail@robin-gueldenpfennig.de>
2021-02-10 19:07:40 +0100

    Complete homework of Module03-AsyncBasics

--- Module03-AsyncBasics/AsyncAwaitExercises.Core/AsyncHelpers.cs
+++ Module03-AsyncBasics/AsyncAwaitExercises.Core/AsyncHelpers.cs
@@ -7,7 +7,7 @@ namespace AsyncAwaitExercises.Core
 {
     public class AsyncHelpers
     {
-        public static async Task<string> GetStringWithRetries(HttpClient client, string url, int maxTries = 3, CancellationToken token = default)
+        public static Task<string> GetStringWithRetries(HttpClient client, string url, int maxTries = 3, CancellationToken token = default)
         {
             // Create a method that will try to get a response from a given `url`, retrying `maxTries` number of times.
             // It should wait one second before the second try, and double the wait time before every successive retry
@@ -22,8 +22,39 @@ namespace AsyncAwaitExercises.Core
             // * `HttpClient.GetStringAsync` does not accept cancellation token (use `GetAsync` instead)
             // * you may use `EnsureSuccessStatusCode()` method
 
-            return string.Empty;
+            if (maxTries <= 2) throw new ArgumentException(nameof(maxTries));
+            if (token.IsCancellationRequested) throw new TaskCanceledException(null, null, token);
+
+            return GetStringWithRetriesInternal(client, url, maxTries, token);
         }
 
+        public static async Task<string> GetStringWithRetriesInternal(
+            HttpClient client, string url, int maxTries = 3, CancellationToken token = default)
+        {
+            int delay = 1000;
+            for (int i = 0; i < maxTries; i++)
+            {
+                try
+                {
+                    var response = await client.GetAsync(url, token);
+                    response.EnsureSuccessStatusCode();
+
+                    return await response.Content.ReadAsStringAsync();
+                }
+                catch (OperationCanceledException e)
+                {
+                    throw new TaskCanceledException(null, e, token);
+                }
+                catch (Exception)
+                {
+                    if (i == maxTries - 1) throw;
+
+                    await Task.Delay(delay, token);
+                    delay *= 2;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
     }
 }
